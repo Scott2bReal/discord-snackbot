325934f94311662a89a8be8b4a0f3b3e3de0d71b
Scott Hoecker <hoeckers@gmail.com>
2023-01-04 08:11:39 -0600

    Add new select menu

--- src/utils/modals.ts
+++ src/utils/modals.ts
@@ -40,8 +40,8 @@ export const addShowModal = {
           custom_id: 'venueName',
           label: 'Venue Name',
           style: 1,
-        }
-      ]
+        },
+      ],
     },
     {
       type: 1,
@@ -52,8 +52,8 @@ export const addShowModal = {
           label: 'Subtitle',
           style: 1,
           required: false,
-        }
-      ]
+        },
+      ],
     },
     {
       type: 1,
@@ -64,8 +64,8 @@ export const addShowModal = {
           label: 'Date (please enter in format YYYY-MM-DD)',
           style: 1,
           required: false,
-        }
-      ]
+        },
+      ],
     },
     {
       type: 1,
@@ -75,8 +75,8 @@ export const addShowModal = {
           custom_id: 'location',
           label: 'Location (enter like "Chicago, IL)',
           style: 1,
-        }
-      ]
+        },
+      ],
     },
     {
       type: 1,
@@ -87,8 +87,55 @@ export const addShowModal = {
           label: 'Ticket Link',
           style: 1,
           required: false,
-        }
-      ]
+        },
+      ],
     },
-  ]
+  ],
+}
+
+export const removeShowMenu = {
+  content: 'Pick a show to remove',
+  components: [
+    {
+      type: 1,
+      components: [
+        {
+          type: 3,
+          custom_id: 'class_select_1',
+          options: [
+            {
+              label: 'Rogue',
+              value: 'rogue',
+              description: 'Sneak n stab',
+              emoji: {
+                name: 'rogue',
+                id: '625891304148303894',
+              },
+            },
+            {
+              label: 'Mage',
+              value: 'mage',
+              description: "Turn 'em into a sheep",
+              emoji: {
+                name: 'mage',
+                id: '625891304081063986',
+              },
+            },
+            {
+              label: 'Priest',
+              value: 'priest',
+              description: "You get heals when I'm done doing damage",
+              emoji: {
+                name: 'priest',
+                id: '625891303795982337',
+              },
+            },
+          ],
+          placeholder: 'Choose a class',
+          min_values: 1,
+          max_values: 3,
+        },
+      ],
+    },
+  ],
 }
