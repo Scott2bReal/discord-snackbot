eb683aa304d2e5bd98a525cf84d4097b5d1696f3
Scott Hoecker <hoeckers@gmail.com>
2023-01-04 14:19:10 -0600

    Use discord ID as id in DB

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,38 +3,28 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const userSeed = [
-  {
-    discordId: '463823197356294156',
-    userName: ' Caleb M',
-  },
-  {
-    discordId: '628079232819658752',
-    userName: ' Scott2bReal',
-  },
-  {
-    discordId: '1060014844008480828',
-    userName: ' Keith B',
-  },
-  {
-    discordId: '1059893151273341019',
-    userName: ' ryangac',
-  },
+  // {
+  //   id: '463823197356294156',
+  //   userName: ' Caleb M',
+  // },
+  // {
+  //   id: '628079232819658752',
+  //   userName: ' Scott2bReal',
+  // },
+  // {
+  //   id: '1060014844008480828',
+  //   userName: ' Keith B',
+  // },
+  // {
+  //   id: '1059893151273341019',
+  //   userName: ' ryangac',
+  // },
 ]
 
 async function main() {
-  for(const user of userSeed) {
-    await prisma.user.create({
-      data: user,
-    })
-  }
-  // for (const climber of realClimberSeed) {
-  //   await prisma.climber.create({
-  //     data: climber,
-  //   })
-  // }
-  // for (const climbingClass of climbingClassSeed) {
-  //   await prisma.climbingClass.create({
-  //     data: climbingClass,
+  // for(const user of userSeed) {
+  //   await prisma.user.create({
+  //     data: user,
   //   })
   // }
 }
