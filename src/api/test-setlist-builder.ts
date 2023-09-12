95687c1e87e41ddc1e3bddf5d0819ae770158397
Scott Hoecker <hoeckers@gmail.com>
2023-09-12 01:01:29 -0500

    Setup test endpoint for setlist builder

---
+++ src/api/test-setlist-builder.ts
@@ -0,0 +1,20 @@
+import { VercelRequest, VercelResponse } from "@vercel/node"
+
+const secretKey = process.env.SETLIST_BUILDER_SECRET
+
+export default async function (req: VercelRequest, res: VercelResponse) {
+  const { authorization } = req.headers
+  if (!(authorization?.split(" ")[1] === secretKey)) {
+    return res.status(401).send("Unauthorized")
+  }
+
+  if (req.method === "GET") {
+    return res.status(200).send("Hello")
+  }
+
+  if (req.method === "POST") {
+    const { message } = req.body
+    console.log(`Received message: ${JSON.stringify(message, null, 2)}`)
+    return res.status(200).send(`You said: ${message}`)
+  }
+}
